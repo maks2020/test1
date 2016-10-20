90f3ff004adcb197706d8cde90f43dfeeaa61752
Maksim Spiridonov <wrkwrtr@gmail.com>
2016-10-20 22:31:55 +0400

    done contact page add menu Contact Details and interactive in social button in sidebar

--- site/contact.php
+++ site/contact.php
@@ -9,25 +9,47 @@
 					<div id="ul_aside_div">
 						
 						<ul class="c1">
-							<li><div class="li_margin_aside">1234 Street Name</div></li>
-							<li><div class="li_margin_aside">City 12345</div></li>
-							<li><div class="li_margin_aside">Country</div></li>
+							<li><div>1234 Street Name</div></li>
+							<li><div>City 12345</div></li>
+							<li><div>Country</div></li>
 						</ul>
 						
 					</div> <!--end ul_aside_div-->
 				
 				</div> <!--end company_info-->
 				
+				
+				<div id="contact_details">
+				
+						
+					<h2 class="header_aside">Contact Details</h2>
+					
+					<div id="ul_aside_div_second">
+						
+						<ul class="c1">
+							<li><div>contact@mail.com</div></li>
+							<li><div>Website: www.Business.com</div></li>
+							<li><div>Phone: 123-456-7890</div></li>
+						</ul>
+						
+					</div> <!--end ul_aside_div-->
+				
+				</div> <!--end contact_details-->
+				
+				
 				<div id="contact_with_us">
 					
 					<h2 class="header_aside">Contact With Us</h2>
 				
 					<div id="ref_social_aside">
-					<ul>
+					<!--<ul>
 						<li><img src="images/in_sidebar_contact.png" alt="image in"></li>
 						<li><img src="images/twitter_sidebar_contact.png" alt="image twitter"></li>
 						<li><img src="images/facebook_sidebar_contact.png" alt="image facebook"></li>
-					</ul>
+					</ul>-->
+					<div id="in_but" class="social_buttons"><img src="images/in_sidebar_contact.png" alt="image in"></div>
+					<div id="twitter_but" class="social_buttons"><img src="images/twitter_sidebar_contact.png" alt="image twitter"></div>
+					<div id="facebook_but" class="social_buttons"><img src="images/facebook_sidebar_contact.png" alt="image facebook"></div>
 					</div>  <!--end ref_social_aside-->
 					
 				</div> <!--end our_clients-->
@@ -50,7 +72,7 @@
 					</div>
 					<div class="value_form">Message:</div>
 					<div class="border_design_textarea">
-					<textarea id="text_area" name="message" class="form_config_contact">Your message</textarea>
+					<textarea id="text_area" name="message" class="form_config_contact"></textarea>
 					</div>
 					<input id="submit_form" type="submit" value="SUBMIT">
 				</form>
