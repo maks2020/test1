71120d03ca46c7216e6b47fbcddd5311d6ba79e5
Maksim Spiridonov <wrkwrtr@gmail.com>
2016-10-20 19:22:12 +0400

    done contact page without one position into aside and send method not added

--- site/contact.php
+++ site/contact.php
@@ -4,18 +4,14 @@
 				<div id="company_info">
 				
 						
-					<h2 class="header_aside">Company Info</h2>
+					<h2 id="header_company_info" class="header_aside">Company Info</h2>
 					
 					<div id="ul_aside_div">
 						
 						<ul class="c1">
-							<li><span class="li_margin_aside">1234 Street Name</span></li>
-							<!--<li>...........................................................</li>-->							
-							<li><span class="li_margin_aside">City 12345</span></li>
-							<!--<li>...........................................................</li>-->
-							<li><span class="li_margin_aside">Country</span></li>
-							<!--<li>...........................................................</li>-->
-							<li><span class="li_margin_aside">Employment</span></li>
+							<li><div class="li_margin_aside">1234 Street Name</div></li>
+							<li><div class="li_margin_aside">City 12345</div></li>
+							<li><div class="li_margin_aside">Country</div></li>
 						</ul>
 						
 					</div> <!--end ul_aside_div-->
@@ -24,34 +20,39 @@
 				
 				<div id="contact_with_us">
 					
-					<h2 class="header_aside">Conect With Us</h2>
+					<h2 class="header_aside">Contact With Us</h2>
 				
-					<div id="aside_img">
-					<ul id="ref_social_aside">
+					<div id="ref_social_aside">
+					<ul>
 						<li><img src="images/in_sidebar_contact.png" alt="image in"></li>
 						<li><img src="images/twitter_sidebar_contact.png" alt="image twitter"></li>
 						<li><img src="images/facebook_sidebar_contact.png" alt="image facebook"></li>
 					</ul>
+					</div>  <!--end ref_social_aside-->
 					
-					</div> <!--end aside_img-->
-				
 				</div> <!--end our_clients-->
 				
 		</aside>
 						
 		<div id="contentContact">
 				<form id="contact_form">
-					
-					<span class="value_form">Name:</span> <br>
-					<input type="text" name="name" class="form_config_contact"><br>
-					<span class="value_form">Email Address:</span><br>
-					<input type="email" name="email" class="form_config_contact"><br>
-					<span class="value_form">Subject:</span><br>
-					<input type="text" name="subject" class="form_config_contact"><br>
-					<span class="value_form">Message:</span><br>
-					<textarea id="text_area" name="message" class="form_config_contact">Your message</textarea><br>
-					<input type="submit" value="Submit">
-					
+					<div class="value_form">Name:</div>
+					<div class="border_design">
+					<input type="text" name="name" class="form_config_contact">
+					</div>
+					<div class="value_form">Email Address:</div>
+					<div class="border_design">
+						<input type="email" name="email" class="form_config_contact">
+					</div>
+					<div class="value_form">Subject:</div>
+					<div class="border_design">
+						<input type="text" name="subject" class="form_config_contact">
+					</div>
+					<div class="value_form">Message:</div>
+					<div class="border_design_textarea">
+					<textarea id="text_area" name="message" class="form_config_contact">Your message</textarea>
+					</div>
+					<input id="submit_form" type="submit" value="SUBMIT">
 				</form>
 										
 		</div> <!--end contentContact-->
